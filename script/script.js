d562972734f4866f0d4def4160b976f6f38f746d
Lemer Marc <105846889+ExiaEarth@users.noreply.github.com>
2023-04-03 21:48:59 +0200

    finition btn +ajout bnt claire et debut presentation

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function switchColor(){
+    // let r=250;
+    // let g=250;
+    // let b=250;
+    // document.body.style.backgroundColor="rgb("+r+","+g+","+b+")"
+    document.body.style.backgroundColor=rgb(255, 255, 255)
+
+};
