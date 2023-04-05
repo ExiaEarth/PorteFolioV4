4859dd3d23fa65efaf1b97728cad860b2f97d8b4
Lemer Marc <105846889+ExiaEarth@users.noreply.github.com>
2023-04-05 20:05:15 +0200

    1v accueil fini et debut de partie contact 05/04/23

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
-function switchColor(){
-    // let r=250;
-    // let g=250;
-    // let b=250;
-    // document.body.style.backgroundColor="rgb("+r+","+g+","+b+")"
-    document.body.style.backgroundColor=rgb(255, 255, 255)
+// function switchColor(){
+//     // let r=250;
+//     // let g=250;
+//     // let b=250;
+//     // document.body.style.backgroundColor="rgb("+r+","+g+","+b+")"
+//     document.body.style.backgroundColor=rgb(255, 255, 255)
+// }
 
-};
+    // const tl=gsap.timeline();
+    // tl.fronTo(".boitefront",{opacity:1},{opacity:0})
+    const tl = gsap.timeline();
+
+tl.from("boitefront",{duration:1,opacity:0,stagger:0.25,});
+tl.to("boitefront",{duration:0.5,y:"20%",opacity:0,stagger});
+
+const tlSid=gsap.timeline();
+tlSid.from("")
