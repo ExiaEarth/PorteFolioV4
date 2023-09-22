449c8f222e7fc010ab43a70898998831152694d0
Lemer Marc <105846889+ExiaEarth@users.noreply.github.com>
2023-09-22 17:51:15 +0200

    maj 22-09-23

--- script/scripteclairage.js
+++ script/scripteclairage.js
@@ -1,13 +1,20 @@
-
+let btnCv= document.getElementById("togg1");
+let btnClaire= document.getElementById("btnNav");
+let btnSombre= document.getElementById("btnNav2");
 
 
 
 function modeClair(color) { 
     document.body.style.background = color="white";
     document.body.style.color="black";
+    btnCv.style.background=color="white";
+    btnSombre.body.style.display="none";
+
     // document.body
 }
 function modeSombre(color) { 
     document.body.style.background = color="black";
     document.body.style.color="aquamarine";
+    btnCv.style.background=color="black";
+    btnClaire.body.style.display="none";
 }
