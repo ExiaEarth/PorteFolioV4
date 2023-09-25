655a1b8ab07a1e2b7286d28774179cb7b74126af
Lemer Marc <105846889+ExiaEarth@users.noreply.github.com>
2023-09-25 15:33:17 +0200

    maj 25/09/23

--- script/scripteclairage.js
+++ script/scripteclairage.js
@@ -1,6 +1,7 @@
 let btnCv= document.getElementById("togg1");
 let btnClaire= document.getElementById("btnNav");
 let btnSombre= document.getElementById("btnNav2");
+let textprojet= document.getElementById('footer1');
 
 
 
@@ -9,6 +10,7 @@ function modeClair(color) {
     document.body.style.color="black";
     btnCv.style.background=color="white";
     btnSombre.body.style.display="none";
+    textprojet.style.color="black";
 
     // document.body
 }
@@ -17,4 +19,5 @@ function modeSombre(color) {
     document.body.style.color="aquamarine";
     btnCv.style.background=color="black";
     btnClaire.body.style.display="none";
+    textprojet.style.color="white";
 }
